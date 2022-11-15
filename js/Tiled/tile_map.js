ff66060b30465db31fa35cfb1aea233905ed118a
eomseung-yeol <co6747@naver.com>
2022-11-15 13:11:09 +0900

    feat: update tileMap

--- js/Tiled/tile_map.js
+++ js/Tiled/tile_map.js
@@ -16,8 +16,8 @@ export default class tile_map extends Phaser.Scene{
 
         // 타일맵 생성.
         // this.load.image('tiles','resource/images/terrain_atlas.png');
-        this.load.image('tiles','resource/images/tileset_school.png');
-        this.load.tilemapTiledJSON('map','resource/images/tiles_map.json');
+        this.load.image('tiles','resource/images/hs-map.png');
+        this.load.tilemapTiledJSON('map','resource/images/square_map.json');
     }
     
     // phaser 내부에서 게임 개체 정의. 
@@ -26,11 +26,13 @@ export default class tile_map extends Phaser.Scene{
 
        
         const map = this.make.tilemap({key:'map'});
-        const tileset = map.addTilesetImage('tileset_school','tiles',32,32,0,0);
+        const tileset = map.addTilesetImage('hs-map','tiles',32,32,0,0);
         const layer1 = map.createStaticLayer('background',tileset,0,0); // background layer
         const layer2 = map.createLayer('interactive',tileset,0,0); // background layer 위에 올려진 layer를 추가할 때, 다음과 같이 구성. => interactive는 json 파일에 layer name으로 명시.  
+        layer1.setCollisionByProperty({collides:true}); // layer1 의 tile의 collides 의 값의 충돌지정.
+        this.matter.world.convertTilemapLayer(layer1);
 
-        this.player = new player({scene:this,x:0,y:0,texture:'obj_man',frame:'townsfolk_m_idle_1'}); // matter base sprite object 생성. 
+        this.player = new player({scene:this,x:100,y:100,texture:'obj_man',frame:'townsfolk_m_idle_1'}); // matter base sprite object 생성.
         this.cameras.main.startFollow(this.player);
         // this.add.existing(this.player);
         this.player.inputKeys = this.input.keyboard.addKeys({ // input 타입은 keyboard 사용하며, 각 input의 키 값 정의.
