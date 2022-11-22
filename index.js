eec139c90679953af4d2bd8e31204a72a53854cc
eomseung-yeol <co6747@naver.com>
2022-11-22 16:14:12 +0900

    feat: apply tailwindcss

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// import Slopes from 'phaser-slopes';
-// import Phaser from 'phaser';
-// import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin';
-
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname+'/public'));
-// app.use(express.static(__dirname+'/test'));
+app.use(express.static(__dirname+'/dist'));
+
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
-
-
 
 app.listen(7500,()=>{
     console.log("server run");
