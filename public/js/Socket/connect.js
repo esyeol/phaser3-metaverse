13b110a229618b64d44d28c0f904d39775546d6b
eomseung-yeol <co6747@naver.com>
2022-11-30 18:04:24 +0900

    fix:socket.io  cors error

--- public/js/Socket/connect.js
+++ public/js/Socket/connect.js
@@ -1,14 +1,22 @@
-const socket = io('ws://3.39.249.46:8080');
-//eyJhbGciOiJzaGEyNTYiLCJ0eXAiOiJKV1QifS57IlVzZXJfSUQiOiJNVEV3IiwiVV9
-//OYW1lIjoiYUc5dVp6UT0iLCJVX0VtYWlsIjoiYUc5dVp6UkFibUYyWlhJdVkyOXQifS4
-//yN2Y0Nzg5NjczYjFlNWRkYzZmNzRhZTI2NDRmNDQ4YmUzNzhkZmNjZmI0YzNmNTM3MzEyNTAzNTMzOTBkYjJi
-
-socket.on('connect',()=> {
-    console.log(data.comment);
+// socket 서버 요청. cors error 위해 임시로 지정. 
+const socket = io('ws://3.39.249.46:8080',{
+    cors: { origin: '*' }
 });
 
-socket.emit("jwt",{data:"eyJhbGciOiJzaGEyNTYiLCJ0eXAiOiJKV1QifS57IlVzZXJfSUQiOiJNVEV3IiwiVV9OYW1lIjoiYUc5dVp6UT0iLCJVX0VtYWlsIjoiYUc5dVp6UkFibUYyWlhJdVkyOXQifS4yN2Y0Nzg5NjczYjFlNWRkYzZmNzRhZTI2NDRmNDQ4YmUzNzhkZmNjZmI0YzNmNTM3MzEyNTAzNTMzOTBkYjJi"});
-socket.on('response', req => {
-    console.log(req);
-})
+socket.on('connect',()=> {
+    console.log('test');
+});
+
+// jwt 토큰값 소켓 서버로 전송.
+socket.emit("joinMetaverse","eyJhbGciOiJzaGEyNTYiLCJ0eXAiOiJKV1QifS57IlVzZXJfSUQiOiJNVEV3IiwiVV9OYW1lIjoiYUc5dVp6UT0iLCJVX0VtYWlsIjoiYUc5dVp6UkFibUYyWlhJdVkyOXQifS4yN2Y0Nzg5NjczYjFlNWRkYzZmNzRhZTI2NDRmNDQ4YmUzNzhkZmNjZmI0YzNmNTM3MzEyNTAzNTMzOTBkYjJi");
+
+/**
+ * 복호화된 paramater 값을 인자로 소켓서버에서 리턴받음. 
+ * @param {id} jwt 토큰에서 복호화한 user 테이블의 id value. 
+ * @param {eamil} jwt 토큰에서 복호화한 user 테이블의 email vlaue.
+ */
+socket.on('userData', (id,eamil) => {   
+    console.log(id);
+    console.log(eamil);
+});
 
