7a3fa58a43dd161d13f84c84180c600031503f15
eomseung-yeol <co6747@naver.com>
2022-12-06 18:49:18 +0900

    feat: Implement character text follow function

--- public/js/Object/player.js
+++ public/js/Object/player.js
@@ -13,13 +13,16 @@ export default class player {
         // this.players = {};  // 소켓에 연결중인 player를 objecdt 형식으로 저장.
 
         // // 캐릭터 지정.
-        this.sprite = scene.physics.add.sprite(x,y,I_PLAYER).setSize(30, 40).setOffset(0, 24);
+        this.sprite = scene.physics.add.sprite(x,y,I_PLAYER).setSize(30, 30).setOffset(10, 24);
         this.keys = scene.input.keyboard.createCursorKeys();
 
         this.scene.cameras.main.fadeFrom(1000);
-        // this.scene.physics.world.setBounds(0, 0, this.scene.map.widthInPixels, this.scene.map.heightInPixels);
-        // this.scene.cameras.main.setBounds(0, 0, this.scene.map.widthInPixels, this.scene.map.heightInPixels);
+        this.scene.physics.world.setBounds(0, 0, this.scene.widthInPixels, this.scene.heightInPixels);
+        this.scene.cameras.main.setBounds(0, 0, this.scene.widthInPixels, this.scene.heightInPixels);
         this.scene.cameras.main.startFollow(this.sprite,true);
+
+        // 유저의 이름을 보여주는 text 팔로우 하도록 설정.
+        this.text = this.scene.add.text(x - 22, y - 45,'닉네임',{ color: '#000000', fontStyle:'bold'}); 
     }
 
     // 캐릭터가 이동함에 따라 값을 변경해주는 로직. 
@@ -28,6 +31,10 @@ export default class player {
         const {keys,sprite} = this;
         const speed = 500;
         const prevVelocity = sprite.body.velocity.clone();
+
+         // 유저 이름과 캐릭터 동시에 이동하도록 지정하는 값 
+         this.text.x = sprite.body.position.x-22; 
+         this.text.y = sprite.body.position.y-45;
 
         sprite.body.setVelocity(0); //  player가 정지했을 때, 마지막 프레임 형태로 정지함. 
 
@@ -52,9 +59,8 @@ export default class player {
         else if(keys.down.isDown){
             sprite.body.setVelocityY(speed);
             sprite.body.setVelocityX(0);            
-                   
+          
         }
-
         sprite.body.velocity.normalize().scale(speed);
 
         if(keys.left.isDown){
@@ -67,9 +73,11 @@ export default class player {
             sprite.anims.play('up', true);
         }else{
             sprite.anims.stop();
-               // If we were moving, pick and idle frame to use
-            //    if (prevVelocity.y < 0) sprite.setTexture("player", 0);
-            //    else sprite.setTexture("player", 46);
+
+            // sprite animation 정지시, default 방향 0 으로 지정.
+            // If we were moving, pick and idle frame to use
+            //    if (prevVelocity.y < 0) sprite.setTexture(I_PLAYER, 0);
+            //    else sprite.setTexture(I_PLAYER, 0);
         } 
     }
 
