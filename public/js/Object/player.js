445e7c214e79b94da880ec2d3fdeb0d59b065e8f
eomseung-yeol <co6747@naver.com>
2022-12-13 00:05:26 +0900

    refactor: 플레이어 이동시  버벅임 수정 & 각 방향에 대한 정의 메서드화

--- public/js/Object/player.js
+++ public/js/Object/player.js
@@ -1,84 +1,84 @@
-/** 소켓 클라이언트에 대한 명시가 여기에 추가되어야함. */ 
-import {I_PLAYER} from "../constants/assets.js";
+/** 소켓 클라이언트에 대한 명시가 여기에 추가되어야함. */
+import { I_PLAYER } from "../constants/assets.js";
 
 export default class player {
+  constructor(scene, x, y, name) {
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    this.name = name;
+
+    //소켓서버 파트가 정해질 경우, 추가.
+    // this.players = {};  // 소켓에 연결중인 player를 objecdt 형식으로 저장.
+
+    // // 캐릭터 지정.
+    this.sprite = scene.physics.add
+      .sprite(this.x, this.y, I_PLAYER)
+      .setSize(30, 30)
+      .setOffset(10, 24);
+    this.keys = scene.input.keyboard.createCursorKeys();
+
+    this.scene.cameras.main.fadeFrom(1000);
+    this.scene.physics.world.setBounds(0,0,this.scene.widthInPixels,this.scene.heightInPixels);
+    this.scene.cameras.main.setBounds(0,0,this.scene.widthInPixels,this.scene.heightInPixels);
+    this.scene.cameras.main.startFollow(this.sprite, true);
+    this.sprite.setCollideWorldBounds(true);
+    this.scene.cameras.main.centerOn(0, 0);
+
+    // 유저의 이름을 보여주는 text 팔로우 하도록 설정.
+    this.text = this.scene.add.text(x - 22, y - 45, this.name, {color: "#000000",fontStyle: "bold",});
+  }
+
+  create(){}
+
+  left() {
+    this.sprite.body.velocity.x = -300;
+    this.sprite.body.velocity.y = -0;
+    this.sprite.anims.play("left", true);
+  }
+
+  right() {
+    this.sprite.body.velocity.x = 300;
+    this.sprite.body.velocity.y = 0;
+    this.sprite.anims.play("right", true);
+  }
+
+  up() {
+    this.sprite.body.velocity.x = 0;
+    this.sprite.body.velocity.y = -300;
+    this.sprite.anims.play("up", true);
+  }
+
+  down() {
+    this.sprite.body.velocity.x = 0;
+    this.sprite.body.velocity.y = 300;
+    this.sprite.anims.play("down", true);
+  }
+
+  stop() {
+    this.sprite.body.velocity.x = 0;
+    this.sprite.body.velocity.y = 0;
+    this.sprite.anims.stop();
+  }
+
+  // 캐릭터가 이동함에 따라 값을 변경해주는 로직.
+  update(direction) {
     
-    constructor(scene,x,y,charactor){
-        this.scene = scene;
-        this.x = x;
-        this.y = y;
-        this.charactor = charactor; 
+    const { isUp, isDown, isLeft, isRight } = direction;
 
-        //소켓서버 파트가 정해질 경우, 추가.
-        // this.players = {};  // 소켓에 연결중인 player를 objecdt 형식으로 저장.
+    this.text.x = this.sprite.body.position.x - 22;
+    this.text.y = this.sprite.body.position.y - 45;
 
-        // // 캐릭터 지정.
-        this.sprite = scene.physics.add.sprite(x,y,I_PLAYER).setSize(30, 30).setOffset(10, 24);
-        this.keys = scene.input.keyboard.createCursorKeys();
-
-        this.scene.cameras.main.fadeFrom(1000);
-        this.scene.physics.world.setBounds(0, 0, this.scene.widthInPixels, this.scene.heightInPixels);
-        this.scene.cameras.main.setBounds(0, 0, this.scene.widthInPixels, this.scene.heightInPixels);
-        this.scene.cameras.main.startFollow(this.sprite,true);
-
-        // 유저의 이름을 보여주는 text 팔로우 하도록 설정.
-        this.text = this.scene.add.text(x - 22, y - 45,'닉네임',{ color: '#000000', fontStyle:'bold'}); 
+    if (isUp) {
+      this.up();
+    } else if (isDown) {
+      this.down();
+    } else if (isLeft) {
+      this.left();
+    } else if (isRight) {
+      this.right();
+    } else {
+      this.stop();
     }
-
-    // 캐릭터가 이동함에 따라 값을 변경해주는 로직. 
-    update(direction){
-
-        const {keys,sprite} = this;
-        const speed = 500;
-        const prevVelocity = sprite.body.velocity.clone();
-
-         // 유저 이름과 캐릭터 동시에 이동하도록 지정하는 값 
-         this.text.x = sprite.body.position.x-22; 
-         this.text.y = sprite.body.position.y-45;
-
-        sprite.body.setVelocity(0); //  player가 정지했을 때, 마지막 프레임 형태로 정지함. 
-
-        if (keys.left.isDown)
-		{
-            sprite.body.setVelocityX(-speed);
-            // sprite.body.setVelocityY(0);
-		}
-		else if (keys.right.isDown)
-		{
-            sprite.body.setVelocityX(speed);
-            sprite.setFlipX(false);
-            // sprite.setVelocityY(0);
-		}
-
-        if(keys.up.isDown)
-		{
-            sprite.body.setVelocityY(-speed);
-            sprite.body.setVelocityX(0);
-         	
-        } 
-        else if(keys.down.isDown){
-            sprite.body.setVelocityY(speed);
-            sprite.body.setVelocityX(0);            
-          
-        }
-        sprite.body.velocity.normalize().scale(speed);
-
-        if(keys.left.isDown){
-            sprite.anims.play('left', true);
-        }else if(keys.right.isDown){
-            sprite.anims.play('right', true);
-        }else if(keys.down.isDown){
-            sprite.anims.play('down', true);
-        }else if(keys.up.isDown){
-            sprite.anims.play('up', true);
-        }else{
-            sprite.anims.stop();
-
-            // sprite animation 정지시, default 방향 0 으로 지정.
-            // If we were moving, pick and idle frame to use
-            //    if (prevVelocity.y < 0) sprite.setTexture(I_PLAYER, 0);
-            //    else sprite.setTexture(I_PLAYER, 0);
-        } 
-    }
-
+  }
 }
