8133c54c3eb5c685d17152df3adf939faecd7b37
eomseung-yeol <co6747@naver.com>
2022-12-12 16:31:23 +0900

    chore: drawing ui seperated

--- public/js/Tiled/classroom.js
+++ public/js/Tiled/classroom.js
@@ -1,6 +1,7 @@
 import player from "../Object/player.js";
 import {Square,ClassRoom} from "../constants/scenes.js";
 import {I_PLAYER, I_CLASSROOM, I_SQUARE, I_PORTAL, M_SQUARE,M_CLASSROOM,I_BOARD} from "../constants/assets.js";
+import drawing from "../Ui/drawBoard.js";
 
 export default class classroom extends Phaser.Scene{
     constructor(){
@@ -81,7 +82,7 @@ export default class classroom extends Phaser.Scene{
         // D 버튼 클릭시 판서 기능 활성화.
         this.input.keyboard.addListener("keydown-K",(event) => {
             document.querySelector('#board-menu').classList.remove('hidden');
-            this.draw();
+            drawing();
            
         });
 
@@ -168,93 +169,93 @@ export default class classroom extends Phaser.Scene{
 //     this.createSpeechBubble(this.board.x,this.board.y,100,50,'M 버튼을 눌러보세요');
 //   }
 
-  // 판서기능 
-  draw(){
-    const writeBtn = document.querySelector('#write');
-    const selectColor = document.querySelector('#select-color');
-    const eraseBtn = document.querySelector('#erase');
-    const destroyBtn = document.querySelector('#destroy');
-    const lineWidth = document.querySelector('#line-width');
-    const canvas = document.querySelector("#drawing-board");
-    const ctx = canvas.getContext('2d');
-    canvas.width = 600;
-    canvas.height = 500;
+  // // 판서기능 
+  // draw(){
+  //   const writeBtn = document.querySelector('#write');
+  //   const selectColor = document.querySelector('#select-color');
+  //   const eraseBtn = document.querySelector('#erase');
+  //   const destroyBtn = document.querySelector('#destroy');
+  //   const lineWidth = document.querySelector('#line-width');
+  //   const canvas = document.querySelector("#drawing-board");
+  //   const ctx = canvas.getContext('2d');
+  //   canvas.width = 600;
+  //   canvas.height = 500;
  
-    ctx.lineWidth=lineWidth.value;
-    ctx.moveTo(0,0);
+  //   ctx.lineWidth=lineWidth.value;
+  //   ctx.moveTo(0,0);
 
-    const colors=[
-     "#ff3838",
-     "#ffb8b8",
-     "#c56cf0",
-     "#ff9f1a",
-     "#fff200",
-     "#32ff7e",
-     "#7efff5",
-    ]
-    let isPainting = false;
+  //   const colors=[
+  //    "#ff3838",
+  //    "#ffb8b8",
+  //    "#c56cf0",
+  //    "#ff9f1a",
+  //    "#fff200",
+  //    "#32ff7e",
+  //    "#7efff5",
+  //   ]
+  //   let isPainting = false;
 
-    // mousemove evnet시, isPainting 값에 따라 그냥 마우스만 움직일지, 그림까지 그리며 움직일지 분기로 처리하는 함수.
-    function onMove(event) {
-      if (isPainting) {
-        ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke();
-        return;
-      }
+  //   // mousemove evnet시, isPainting 값에 따라 그냥 마우스만 움직일지, 그림까지 그리며 움직일지 분기로 처리하는 함수.
+  //   function onMove(event) {
+  //     if (isPainting) {
+  //       ctx.lineTo(event.offsetX, event.offsetY);
+  //       ctx.stroke();
+  //       return;
+  //     }
 
-      ctx.beginPath(); // line 초기화
-      ctx.moveTo(event.offsetX, event.offsetY);
-    }
+  //     ctx.beginPath(); // line 초기화
+  //     ctx.moveTo(event.offsetX, event.offsetY);
+  //   }
 
-    //마우스를 클릭하면 isPainting 값이 true 로 바뀌며 onMove에서 그림을 그리는 기능을 수행.
-    function startDrawing(event) {
-      isPainting = true;
-    }
+  //   //마우스를 클릭하면 isPainting 값이 true 로 바뀌며 onMove에서 그림을 그리는 기능을 수행.
+  //   function startDrawing(event) {
+  //     isPainting = true;
+  //   }
 
-    function cancelDrawing() {
-      isPainting = false;
-    }
+  //   function cancelDrawing() {
+  //     isPainting = false;
+  //   }
 
-    //line change 함수
-    function selectLineWidth(event) {
-        ctx.lineWidth = event.target.value;
-    }
+  //   //line change 함수
+  //   function selectLineWidth(event) {
+  //       ctx.lineWidth = event.target.value;
+  //   }
     
-    // canvas 에서 색상을 선택하는 함수.
-    function selectColorDraw(event){
-        ctx.strokeStyle = event.target.value;
-        ctx.fillStyle = event.target.value;
-    }
+  //   // canvas 에서 색상을 선택하는 함수.
+  //   function selectColorDraw(event){
+  //       ctx.strokeStyle = event.target.value;
+  //       ctx.fillStyle = event.target.value;
+  //   }
 
-    // canvas 지우는 함수.
-    function destroyCanvas(){
-        ctx.fillStyle = "white";
-        ctx.fillRect(0,0,600,700);
-    }
+  //   // canvas 지우는 함수.
+  //   function destroyCanvas(){
+  //       ctx.fillStyle = "white";
+  //       ctx.fillRect(0,0,600,700);
+  //   }
 
-    // 지우개 생성.
-    function onEraseClick(){
-        ctx.strokeStyle="white";
-        ctx.fillStyle="white";
-    }
+  //   // 지우개 생성.
+  //   function onEraseClick(){
+  //       ctx.strokeStyle="white";
+  //       ctx.fillStyle="white";
+  //   }
     
-    // canvas draw 활성화 함수.
-    function onWriteDrawing(){
-        ctx.strokeStyle=selectColor.value;
-    }
+  //   // canvas draw 활성화 함수.
+  //   function onWriteDrawing(){
+  //       ctx.strokeStyle=selectColor.value;
+  //   }
 
 
-    //Listener
-    canvas.addEventListener("mousemove", onMove);
-    canvas.addEventListener("mousedown", startDrawing);
-    canvas.addEventListener("mouseup", cancelDrawing);
-    canvas.addEventListener("mouseleave",cancelDrawing);
-    lineWidth.addEventListener("change",selectLineWidth);
-    selectColor.addEventListener("change",selectColorDraw);
-    destroyBtn.addEventListener("click",destroyCanvas);
-    eraseBtn.addEventListener('click',onEraseClick);
-    writeBtn.addEventListener('click',onWriteDrawing);
-  }
+  //   //Listener
+  //   canvas.addEventListener("mousemove", onMove);
+  //   canvas.addEventListener("mousedown", startDrawing);
+  //   canvas.addEventListener("mouseup", cancelDrawing);
+  //   canvas.addEventListener("mouseleave",cancelDrawing);
+  //   lineWidth.addEventListener("change",selectLineWidth);
+  //   selectColor.addEventListener("change",selectColorDraw);
+  //   destroyBtn.addEventListener("click",destroyCanvas);
+  //   eraseBtn.addEventListener('click',onEraseClick);
+  //   writeBtn.addEventListener('click',onWriteDrawing);
+  // }
 
 
     
--- public/js/Ui/canvasRender.js
+++
@@ -1,2 +0,0 @@
-// const canvas = document.getElementsByTagName("canvas")[0];
-// canvas.classList.add('flex','mx-0','place-content-center');
